b4b3f51bf31ce2141bc4c68f84a8398bfa263acc
Dimitar <dimitar.mandarliev@gmail.com>
2024-07-19 12:48:45 +0300

    Sidebar + h-screen issue, react-icons, tailwind merge

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,11 @@
 
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import { HiHome, HiSearch } from "react-icons/hi";
+import { Box } from "./box";
+import { SidebarItem } from "./sidebar-item";
+import { Library } from "./library";
+import { twMerge } from "tailwind-merge";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -11,10 +16,54 @@ export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   const pathname = usePathname();
   const routes = useMemo(
     () => [
-      { label: "Home", active: pathname !== "/search", href: "/" },
-      { label: "Search", active: pathname === "/search", href: "/search" },
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        icon: HiSearch,
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+      },
     ],
     [pathname]
   );
-  return <div>{children}</div>;
+  return (
+    <div
+      className={twMerge(
+        `
+        flex 
+        h-screen
+        `
+      )}
+    >
+      <div
+        className="
+        hidden
+          md:flex 
+          flex-col 
+          gap-y-2 
+          bg-black 
+          h-full 
+          w-[300px] 
+          p-2
+        "
+      >
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
 };
